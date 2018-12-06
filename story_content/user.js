45b69c94255b6100e9af30b4773ef0fa11b5a240
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2018-12-06 12:51:51 +0300

    tncc

---
+++ story_content/user.js
@@ -0,0 +1,30 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5rZYJ5Eiraa":
+        Script1();
+        break;
+      case "5peUzmePavP":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=month + "/" + day + "/" + year
+
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script2()
+{
+  $("#tab-customlink").show();
+}
+
